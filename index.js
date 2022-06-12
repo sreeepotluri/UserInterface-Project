4d284c68012bcd5e49c08b9226221aafd94ac809
sreeepotluri <potlurip1@newpaltz.edu>
2022-06-12 01:52:43 -0400

    installed mongodb and mongoose

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const path = require('path');
+
+mongoose.connect(process.env.dbURL)
+ .then(console.log("DB Connected!!"))
+ .catch(error => console.log(error));
 
 app.use(express.json());
 
 app.use(express.static(__dirname + "/public"));
 app.get('/', (req,res) => res.sendFile(path.join(__dirname, '/public', 'index.html')));
+
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
