4d3a03981709c86dd2e37d48c678d889357d0c12
sreeepotluri <potlurip1@newpaltz.edu>
2022-06-28 20:08:19 -0400

    ADDED PROXY AND CHANGED PORT NO

--- index.js
+++ index.js
@@ -24,5 +24,5 @@ app.use(function(req, res, next) {
     next();
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`server started on port ${PORT}!`));
