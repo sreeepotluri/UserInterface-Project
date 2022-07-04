3667f909c61ea6b8a70d35d86c2b6996f4b29c88
sreeepotluri <potlurip1@newpaltz.edu>
2022-07-03 23:51:38 -0400

    Added Facts Regarding Brands

--- client/src/components/pages/Regarding.js
+++ client/src/components/pages/Regarding.js
@@ -6,7 +6,7 @@ const Regarding = () => {
           <div className="container">
           <hr />
 
-          <h2 className="text-center">FEW FACTS REGARDING BRANDS...</h2>
+          <h2 className="text-center">FACTS REGARDING ADIDAS...</h2>
           <hr />
 
           <div className="row text-primary">
@@ -29,25 +29,23 @@ const Regarding = () => {
            It is the largest sportswear manufacturer in Europe, and the second largest in the world, after Nike.
            </div>
 
-           <button className = "mt-5 btn-danger">View More Products </button>
+           <button className = "mt-5 btn-danger">View Brand Products </button>
 
          </div>
 
          <hr />
 
-         <h2 className="text-center"> FEW MORE FACTS REGARDING BRANDS...</h2>
+         <h2 className="text-center">  FACTS REGARDING PUMA AND REEBOK...</h2>
          <hr />
          <div className="row">
             <div className="col-md bg-warning p-5">
-            Adidas AG is a German multinational corporation, founded and headquartered in Herzogenaurach,
-            Bavaria, that designs and manufactures shoes, clothing and accessories. 
-            It is the largest sportswear manufacturer in Europe, and the second largest in the world, after Nike.
+            Puma SE, branded as Puma, is a German multinational corporation that designs and manufactures athletic and casual footwear, apparel and accessories, which is headquartered in Herzogenaurach, Bavaria, Germany. 
+            Puma is the third largest sportswear manufacturer in the world. The company was founded in 1948 by Rudolf Dassler.
            </div>
 
            <div className="col-md">
-           Adidas AG is a German multinational corporation, founded and headquartered in Herzogenaurach,
-           Bavaria, that designs and manufactures shoes, clothing and accessories. 
-           It is the largest sportswear manufacturer in Europe, and the second largest in the world, after Nike.
+           Reebok International Limited is an American fitness footwear and clothing manufacturer
+           subsidiary of brand management company Authentic Brands Group.
            </div>
 
             </div>
