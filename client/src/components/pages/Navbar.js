285645b32f516940eed9c970eef29fe2eaec0a83
sreeepotluri <potlurip1@newpaltz.edu>
2022-07-05 05:48:17 -0400

    Added Profile Page and provided Links

--- client/src/components/pages/Navbar.js
+++ client/src/components/pages/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
   <div>
    <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
-    <Link className="navbar-brand" to="/">Brand Application</Link>
+    <Link className="navbar-brand" to="/">Profile</Link>
     
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
